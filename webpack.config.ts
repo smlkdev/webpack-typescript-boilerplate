1c9a2c03239eabe42566b25322e8771b1d0c21dd
smlk.dev <1147241-eskaapps@users.noreply.gitlab.com>
2023-05-25 13:21:20 +0200

    add webpack
    Co-authored-by: smlk.dev <48448829+smlkdev@users.noreply.github.com>

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+import * as webpack from "webpack";
+import * as path from "path";
+
+const commonConfig: webpack.Configuration = {
+  entry: {
+    app: "./src/index.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  target: "node",
+};
+
+const devConfig: webpack.Configuration = {
+  ...commonConfig,
+  mode: "development",
+  name: "dev",
+  watch: true,
+};
+
+const prodConfig: webpack.Configuration = {
+  ...commonConfig,
+  mode: "production",
+  name: "prod",
+};
+
+export default [devConfig, prodConfig];
